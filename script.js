0a4a6ecc6a2cc9a97fd74d752e90038a04a455b0
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-03-19 19:32:41 -0300

    Save the first node and refactor code

--- script.js
+++ script.js
@@ -1,62 +1,90 @@
+// Get canvas
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-/* ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(300, 150);
+// Get window size
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
-// Draw the Path
-ctx.stroke(); */
-
-const dotArray  = []
-
-// Função para capturar cliques no canvas
-canvas.addEventListener('click', function(event) {
-    // Obtém as coordenadas do clique em relação ao canvas
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-
-    // Desenha um círculo no local do clique
-    ctx.beginPath();
-    ctx.arc(x, y, 2, 0, Math.PI * 2); // Círculo com raio 10
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.closePath();
-
-    console.log(`Clique em (${x}, ${y})`);
-
-    dotArray.push({x: x, y: y})
-
-    if (dotArray.length === 2) {
-        ctx.moveTo(dotArray[0].x, dotArray[0].y)
-        ctx.lineTo(dotArray[1].x, dotArray[1].y)
-        ctx.strokeStyle = 'black'
-        ctx.stroke();
-        dotArray.shift()
-    }
-});
-
-function drawGrid(ctx, canvas, cellSize = 20, color = '#cccccc57') {
-    // Configurações do grid
+//IIFE (Immediately Invoked Function Expression) to create the canvas grid
+(function DrawGrid() {
+    const cellSize = 20
+    const color = '#cccccc57'
+    // Grid style
     ctx.strokeStyle = color;
     ctx.lineWidth = 1;
 
-    // Desenha linhas verticais
-    for (let x = 0; x <= canvas.width; x += cellSize) {
+    // Draw vertical lines
+    for (let x = 0; x <= windowWidth; x += cellSize) {
         ctx.beginPath();
         ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvas.height);
+        ctx.lineTo(x, windowHeight);
         ctx.stroke();
     }
 
-    // Desenha linhas horizontais
-    for (let y = 0; y <= canvas.height; y += cellSize) {
+    // Draw horizontal lines
+    for (let y = 0; y <= windowHeight; y += cellSize) {
         ctx.beginPath();
         ctx.moveTo(0, y);
-        ctx.lineTo(canvas.width, y);
+        ctx.lineTo(windowWidth, y);
         ctx.stroke();
     }
-}
 
-drawGrid(ctx, canvas)
+    ctx.closePath()
+})();
+
+// Variables
+const canvasArea = canvas.getBoundingClientRect();
+const nodeArray = []
+const firstNode = {
+    x: 0,
+    y: 0
+}
+Object.seal(firstNode)
+
+// Add click event to canvas
+canvas.addEventListener('click', function (event) {
+
+    // Get mouse coordinates
+    const x_pos = event.clientX - canvasArea.left;
+    const y_pos = event.clientY - canvasArea.top;
+
+    // Draw node
+    DrawNode(x_pos, y_pos);
+
+    // Save first node
+    if (nodeArray.length === 0) {
+        firstNode.x = x_pos
+        firstNode.y = y_pos
+    }
+
+    // Push the node position to the array
+    nodeArray.push({ x: x_pos, y: y_pos })
+
+    // If nodeArray has 2 nodes, draw a line and shift the older value
+    if (nodeArray.length === 2) {
+        ctx.beginPath();
+        ctx.moveTo(nodeArray[0].x, nodeArray[0].y)
+        ctx.lineTo(nodeArray[1].x, nodeArray[1].y)
+        ctx.strokeStyle = 'black'
+        ctx.stroke();
+        ctx.closePath();
+        nodeArray.shift()
+    }
+});
+
+/***********************FUNCTIONS***********************/
+
+/**
+ * Draw a node in a given position
+ * 
+ * @param {number} x 
+ * @param {number} y 
+ */
+function DrawNode(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 2, 0, Math.PI * 2);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+}
